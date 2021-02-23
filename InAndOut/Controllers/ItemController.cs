3b441a572139483fa65129b603600f728ca4e16e
user <user@DESKTOP-LC5VIEM>
2021-02-23 18:47:43 +0400

    HelpPrac

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Invoice> objList = _db.Items;
+            return View(objList);
+        }
+
+        // GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Invoice obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
